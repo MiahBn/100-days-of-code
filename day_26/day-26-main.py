53bbb21a4c7dadd98e001aaab75d9efcbe7adc75
Miah Bowen <miah@miahbowen.com>
2023-05-16 01:15:35 -0600

    Wrapping up for the night, committing anything i've added

---
+++ day_26/day-26-main.py
@@ -0,0 +1,11 @@
+import pandas
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+screen.exitonclick()
